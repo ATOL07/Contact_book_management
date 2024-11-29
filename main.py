dd65d20ae88f48f273342fc6c72739e1ec01d295
ATOL07 <khairulatol291@gmail.com>
2024-11-29 22:20:07 +0600

    main.py-created

---
+++ main.py
@@ -0,0 +1,10 @@
+contacts = []
+
+while True:
+    print("\nWelcome to Contact Book Management System")
+    print("0. Exit")
+    print("1. Add Contact")
+    print("2. View All Contacts")
+    print("3. Remove Contact")
+    print("4. Search Contact")
+    print("5. Update Contact")
